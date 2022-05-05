77114fefbf6504ef90ec82256b7f340f4634a744
Ivan2d <93611582+Ivan2d@users.noreply.github.com>
2022-05-05 15:41:55 +0600

    Update Task_4.cpp

--- Task_4/Task_4.cpp
+++ Task_4/Task_4.cpp
@@ -1,22 +1,346 @@
-﻿#include <iostream>
-#include "list"
-
+#include <exception>
+#include <utility>
+#include <list>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-class HashTable 
+template <typename U>
+struct Node {
+    U data;
+    Node* next;
+    Node* prev;
+};
+
+template <typename T>
+class Iterator {
+public:
+    virtual T operator*() = 0;
+    virtual Iterator& operator++() = 0;
+    virtual Iterator& operator--() = 0;
+    virtual bool hasNext() = 0;
+    virtual Node<T>* getCurrent() = 0;
+    virtual void setCurrent(Node<T>* node) = 0;
+};
+
+template <typename T>
+class IList {
+public:
+    virtual void addElem(T elem, Iterator<T>& it) = 0;
+    virtual void deleteElem(Iterator<T>& it) = 0;
+    virtual void clear() = 0;
+    virtual bool empty() = 0;
+    virtual unsigned size() = 0;
+};
+
+template <typename U>
+class LinkedList : public IList<U> {
+public:
+    //Самые важные методы
+    using PNode = Node<U>*; //указатель на узел
+    LinkedList();
+    void addElem(U elem);
+    bool empty();
+    unsigned size();
+    void clear();
+    LinkedList(const LinkedList& obj);
+    LinkedList(LinkedList&& obj) noexcept;
+    LinkedList& operator=(const LinkedList<U>& obj);
+    LinkedList& operator=(LinkedList&& obj) noexcept;
+    ~LinkedList();
+    //Итератор
+    class listIterator : public Iterator<U> {
+    public:
+        listIterator();
+        explicit listIterator(LinkedList<U>& obj);
+        listIterator(LinkedList<U>& obj, PNode& node);
+        U operator*();
+        listIterator& operator++();
+        listIterator operator++(int);
+        listIterator& operator--();
+        listIterator operator--(int);
+        bool operator==(const listIterator& it) const;
+        bool operator!=(const listIterator& it) const;
+        PNode getCurrent();
+        void setCurrent(Node<U>* node);
+        bool hasNext();
+        ~listIterator();
+        friend class LinkedList<U>;
+    private:
+        PNode current; //текущий узел
+        LinkedList<U>* list;
+    };
+    void addElem(U elem, Iterator<U>& it);
+    void deleteElem(Iterator<U>& it);
+    listIterator searchElem(U elem);
+    listIterator begin() { return listIterator(*this); };
+    listIterator end() { return listIterator(); };
+private:
+    PNode head;
+    unsigned len;
+};
+
+template<typename U>
+LinkedList<U>::LinkedList() : head(nullptr), len(0) {}
+
+template<typename U>
+void LinkedList<U>::addElem(U elem) {
+    auto cell = new Node<U>;
+    cell->data = elem;
+    if (head == nullptr) {
+        head = cell;
+        head->next = head;
+        head->prev = head;
+    }
+    else {
+        cell->next = head->next;
+        head->next->prev = cell;
+        head->next = cell;
+        cell->prev = head;
+    }
+    len++;
+}
+
+template<typename U>
+bool LinkedList<U>::empty() {
+    return head == nullptr;
+}
+
+template<typename U>
+unsigned LinkedList<U>::size() {
+    return len;
+}
+
+template<typename U>
+LinkedList<U>::listIterator::listIterator() : current(nullptr), list(nullptr) {}
+
+template<typename U>
+LinkedList<U>::listIterator::listIterator(LinkedList<U>& obj) {
+    if (obj.head != nullptr) {
+        current = obj.head;
+        list = (&obj);
+    }
+}
+
+template<typename U>
+U LinkedList<U>::listIterator::operator*() {
+    return current->data;
+}
+
+template<typename U>
+LinkedList<U>::listIterator::listIterator(LinkedList<U>& obj, PNode& node) {
+    current = node;
+    list = (&obj);
+}
+
+
+template<typename U>
+typename LinkedList<U>::listIterator LinkedList<U>::listIterator::operator++(int) {
+    auto old = (*this);
+    ++(*this);
+    return old;
+}
+
+template<typename U>
+typename LinkedList<U>::listIterator& LinkedList<U>::listIterator::operator--() {
+    if (current->prev == list->head->prev) {
+        current = nullptr;
+    }
+    else {
+        current = current->prev;
+    }
+    return (*this);
+}
+
+template<typename U>
+typename LinkedList<U>::listIterator LinkedList<U>::listIterator::operator--(int) {
+    auto old = (*this);
+    --(*this);
+    return old;
+}
+
+template<typename U>
+bool LinkedList<U>::listIterator::hasNext() {
+    return current->next != list->head;
+}
+
+template<typename U>
+typename LinkedList<U>::listIterator& LinkedList<U>::listIterator::operator++() {
+    current->next == list->head ? current = nullptr : current = current->next;
+    return (*this);
+}
+
+template<typename U>
+typename LinkedList<U>::PNode LinkedList<U>::listIterator::getCurrent() {
+    return current;
+}
+
+template<typename U>
+void LinkedList<U>::listIterator::setCurrent(Node<U>* node) {
+    current = node;
+}
+
+template<typename U>
+bool LinkedList<U>::listIterator::operator==(const LinkedList::listIterator& it) const {
+    if (current == nullptr && it.current == nullptr) {
+        return true;
+    }
+    else if (current == nullptr || it.current == nullptr || it.list == nullptr || list == nullptr) {
+        return false;
+    }
+    else {
+        return current == it.current && list && it.list;
+    }
+}
+
+template<typename U>
+bool LinkedList<U>::listIterator::operator!=(const LinkedList::listIterator& it) const {
+    return !(*this == it);
+}
+
+template<typename U>
+LinkedList<U>::listIterator::~listIterator() {
+    list = nullptr;
+    current = nullptr;
+    delete list;
+    delete current;
+}
+
+template<typename U>
+void LinkedList<U>::addElem(U elem, Iterator<U>& it) {
+    auto cell = new Node<U>;
+    cell->data = elem;
+    cell->next = it.getCurrent()->next;
+    cell->prev = it.getCurrent()->prev;
+    it.getCurrent()->next->prev = cell;
+    it.getCurrent()->next = cell;
+    len++;
+}
+
+template<typename U>
+void LinkedList<U>::deleteElem(Iterator<U>& it) {
+    PNode tempNode = it.getCurrent();
+    if (it.getCurrent()->next != it.getCurrent()) {
+        it.getCurrent()->next->prev = it.getCurrent()->prev;
+        it.getCurrent()->prev->next = it.getCurrent()->next;
+        it.setCurrent(tempNode->next);
+    }
+    len--;
+    delete tempNode;
+}
+
+template<typename U>
+typename LinkedList<U>::listIterator LinkedList<U>::searchElem(U elem) {
+    if (empty()) {
+        throw exception();
+    }
+    PNode temp = head;
+    do {
+        if (temp->data == elem) {
+            return listIterator(*this, temp);
+        }
+        temp = temp->next;
+    } while (temp != head);
+    throw exception();
+}
+
+template<typename U>
+void LinkedList<U>::clear() {
+    PNode it = head->next;
+    while (it != head) {
+        PNode old = it;
+        it = it->next;
+        delete old;
+    }
+    head = nullptr;
+    len = 0;
+}
+
+template<typename U>
+LinkedList<U>::~LinkedList() {
+    if (!empty()) {
+        clear();
+        delete head;
+    }
+
+}
+
+template<typename U>
+LinkedList<U>::LinkedList(const LinkedList& obj) {
+    head = new Node<U>;
+    head->data = obj.head->data;
+    PNode it = head;
+    PNode oldIterator = obj.head->next;
+    while (oldIterator != obj.head) {
+        auto temp = new Node<U>;
+        it->next = temp;
+        temp->prev = it;
+        temp->data = oldIterator->data;
+        it = it->next;
+        oldIterator = oldIterator->next;
+    }
+    head->prev = it;
+    it->next = head;
+    len = obj.len;
+}
+
+template<typename U>
+LinkedList<U>::LinkedList(LinkedList&& obj) noexcept : head(obj.head), len(obj.len) {
+    obj.head = nullptr;
+    obj.len = 0;
+}
+
+template<typename U>
+LinkedList<U>& LinkedList<U>::operator=(const LinkedList<U>& obj) {
+    if (*this == obj) {
+        delete head;
+        head = new Node<U>;
+        head->data = obj.head->data;
+        PNode it = head;
+        PNode oldIterator = obj.head->next;
+        while (oldIterator != obj.head) {
+            auto temp = new Node<U>;
+            it->next = temp;
+            temp->prev = it;
+            temp->data = oldIterator->data;
+            it = it->next;
+            oldIterator = oldIterator->next;
+        }
+        head->prev = it;
+        it->next = head;
+        len = obj.len;
+    }
+    return (*this);
+}
+
+template<typename U>
+LinkedList<U>& LinkedList<U>::operator=(LinkedList&& obj) noexcept {
+    if (*this != obj) {
+        delete head;
+        head = obj.head;
+        len = obj.len;
+        obj.head = nullptr;
+        obj.len = 0;
+    }
+    return (*this);
+}
+
+class HashTable
 {
-    class Entry {
+    class Entry
+    {
     public:
         int key;
         int value;
 
-        Entry(int key, int value) 
+        Entry(int key, int value)
         {
             this->key = key;
             this->value = value;
         }
     };
-    class Iterator 
+
+    class Iterator
     {
     public:
         explicit Iterator(HashTable& ptr) : tableSize(ptr.tableSize), ptr((ptr.pEntry)), id(0) {};
@@ -44,6 +368,7 @@ public:
     bool isEmpty();
     Iterator begin();
     Iterator end();
+    list<int> listik;
 };
 
 HashTable::HashTable() : tableSize(10) {
@@ -60,7 +385,8 @@ int HashTable::hashFunction(int k) const {
     return k % tableSize;
 }
 
-void HashTable::add(int k, int v) {
+void HashTable::add(int k, int v)
+{
     int h = hashFunction(k);
     while (pEntry[h] != nullptr && pEntry[h]->key != k) {
         h = hashFunction(h + 1);
@@ -70,10 +396,14 @@ void HashTable::add(int k, int v) {
         pEntry[h] = nullptr;
     }
     pEntry[h] = new Entry(k, v);
+    Entry* p = new Entry(k,v);
+    listik.push_back(v);
 }
 
-void HashTable::remove(int k) {
+void HashTable::remove(int k)
+{
     int h = hashFunction(k);
+    
     while (pEntry[h] != nullptr) {
         if (pEntry[h]->key == k)
             break;
@@ -131,10 +461,10 @@ HashTable::Iterator HashTable::end() {
     return Iterator(*this, tableSize - 1);
 }
 
-int HashTable::Iterator::operator*() {
-    if (ptr[id] != nullptr) {
+int HashTable::Iterator::operator*()
+{
+    if (ptr[id] != nullptr) 
         return ptr[id]->value;
-    }
 }
 
 bool HashTable::Iterator::operator==(const HashTable::Iterator& other) const {
@@ -149,7 +479,7 @@ HashTable::Iterator& HashTable::Iterator::operator=(const HashTable::Iterator& c
     return *this;
 }
 
-int main() 
+int main()
 {
     HashTable p;
     p.add(10, 14);
@@ -164,7 +494,8 @@ int main()
     p.remove(111);
     cout << p.searchKey(111) << endl;
 
-    for (auto it = p.begin(); it != p.end(); ++it) {
+    cout << endl;
+    for (auto it = p.listik.begin(); it != p.listik.end(); ++it) {
         std::cout << *it << " ";
     }
     return 0;
